fcbf57286d2644518b5e79e2f0733b1c15c83045
wellvolks <wellvolks@hotmail.com>
2023-11-16 02:12:10 -0300

    Primeira versão

--- src/pages/menu/slides.tsx
+++ src/pages/menu/slides.tsx
@@ -1,28 +1,45 @@
-import { Center } from "../../styles/components/Slides.style";
-import Container from 'react-bootstrap/Container';
-import ListSlide from "./slide";
+import Image from "next/image";
+import React, {useState} from "react";
+import Typography from "@mui/material/Typography";
 
 interface IProps {
-    slides: String[];
-    musicoNome: String;
+    slides: string[];
+    musicoNome: string;
 }
 
 const Slides: React.FC<IProps> = ({ slides, musicoNome }) => {
+
+    const [cifraId, setCifraId] = useState(0)
+    const [showSlide, setShowSlide] = useState(false)
+
+    const handleShowSlide = (slideId: number) => {
+        console.log(slideId)
+        setCifraId(slideId)
+        setShowSlide(true)
+    };
+
     return (
-        <Container>
-            <Center>
-                {slides.map((slideInfo, index) => {
+        <div>
+            {showSlide}
+            <h1 onClick={()=>{setShowSlide(false)}}><a href="#"> ÍNDICE </a></h1>
+            <ul style={{columns: 4, listStyleType:"none", WebkitColumns: 4, MozColumns: 4, MozColumnWidth: "300px", WebkitColumnWidth: "300px"}}>
+                {!showSlide && slides && slides.length && slides.map((slideInfo, index) => {
                     return (
-                        <ListSlide
-                            key={index}
-                            slideId={index}
-                            slideNome={slideInfo}
-                            musicoNome={musicoNome}
-                        />
+                        <li key={index} style={{padding: "5px"}}>
+                            <Typography style={{fontSize:"x-large"}}><b>{index + 1}</b> - <button style={{fontSize: "x-large",  cursor: "pointer"}} onClick={() => handleShowSlide(index+2)}>{slideInfo}</button></Typography>
+                        </li>
                     );
                 })}
-            </Center>
-        </Container>
+            </ul>
+            {showSlide && <div>
+                {cifraId && musicoNome && <Image src={require('../../assets/cifras/' + musicoNome + '/' +  cifraId + '.png')} alt={'img'} quality="100" sizes="110vw"
+                                                 style={{
+                                                     width: '70%',
+                                                     height: 'auto',
+                                                 }}	></Image>}
+                </div>
+            }
+        </div>
     );
 };
 
